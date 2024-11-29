49b736c3fea168218d93c4c8c659c79317056863
Hyunji Park <119161420+hhyuun@users.noreply.github.com>
2024-11-29 14:58:10 +0900

    스크랩 삭제, 스크랩된 대화 주제 삭제 - 단위테스트 작성 (#110)
    * test: 스크랩 삭제 API 단위테스트 작성(#109)
    
    * test: 스크랩된 대화주제 삭제 API 단위테스트 작성(#109)
    
    * test: 스크랩 삭제 비즈니스 로직 단위테스트 작성(#109)
    
    * test: 스크랩된 대화주제 삭제 비즈니스 로직 단위테스트 작성(#109)
    
    * feat: 불필요한 import문 정리(#109)
    
    * feat: 클래스 이름 변경(#109)
    
    * chore: CD 스크립트 삭제(#109)
    
    - 테스트 작성 기간 사용하지 않는 CD 스크립트 삭제

--- src/test/java/org/example/tokpik_be/scrap/controller/ScrapControllerTest.java
+++ src/test/java/org/example/tokpik_be/scrap/controller/ScrapControllerTest.java
@@ -2,10 +2,8 @@ package org.example.tokpik_be.scrap.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willDoNothing;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -252,7 +250,7 @@ public class ScrapControllerTest extends ControllerTestSupport {
     @Test
     void getScrapTopicCountSuccess() throws Exception {
         // given
-        long userId = 1;
+        long userId = 1L;
 
         ScrapCountResponse scrapCountResponse = new ScrapCountResponse(20L);
         given(scrapService.getUserTopicCount(userId)).willReturn(scrapCountResponse);
@@ -262,6 +260,105 @@ public class ScrapControllerTest extends ControllerTestSupport {
 
         // then
         resultActions.andExpect(status().isOk());
+    }
+
+    @Nested
+    @DisplayName("스크랩 삭제 시 ")
+    class DeleteScrapTest{
+        
+        @DisplayName("성공한다.")
+        @Test
+        void success() throws Exception {
+            // given
+            long scrapId = 1L;
+            willDoNothing().given(scrapService).deleteScrap(scrapId);
+
+            // when
+            ResultActions resultActions = mockMvc.perform(delete("/users/scraps/{scrapId}", scrapId));
+            
+            // then
+            resultActions.andExpect(status().isOk());
+        }
+        
+        @DisplayName("존재하지 않는 스크랩이면 예외가 발생한다.")
+        @Test
+        void scrapNotFound() throws Exception {
+            // given
+            long scrapId = 1L;
+
+            willThrow(new GeneralException(ScrapException.SCRAP_NOT_FOUND))
+                .given(scrapService).deleteScrap(scrapId);
+            
+            // when
+            ResultActions resultActions = mockMvc.perform(delete("/users/scraps/{scrapId}", scrapId));
+            
+            // then
+            resultActions.andExpect(status().isBadRequest())
+                .andExpect(
+                    jsonPath("$.message").value(ScrapException.SCRAP_NOT_FOUND.getMessage()));
+        }
+    }
+
+    @Nested
+    @DisplayName("스크랩된 대화주제 삭제 시 ")
+    class DeleteScrapTopicTest {
+
+        @DisplayName("성공한다.")
+        @Test
+        void success() throws Exception {
+            // given
+            long scrapId = 1L;
+            long scrapTopicId = 1L;
+
+            willDoNothing().given(scrapService).deleteScrapTopic(scrapId, scrapTopicId);
+
+            // when
+            ResultActions resultActions = mockMvc.perform(
+                delete("/users/scraps/{scrapId}/topics/{scrapTopicId}", scrapId, scrapTopicId));
+
+            // then
+            resultActions.andExpect(status().isOk());
+        }
+
+        @DisplayName("존재하지 않는 스크랩이면 예외가 발생한다.")
+        @Test
+        void scrapNotFound() throws Exception {
+            // given
+            long scrapId = 1L;
+            long scrapTopicId = 1L;
+
+            willThrow(new GeneralException(ScrapException.SCRAP_NOT_FOUND))
+                .given(scrapService).deleteScrapTopic(scrapId, scrapTopicId);
+
+            // when
+            ResultActions resultActions = mockMvc.perform(
+                delete("/users/scraps/{scrapId}/topics/{scrapTopicId}", scrapId, scrapTopicId));
+
+            // then
+            resultActions.andExpect(status().isBadRequest())
+                .andExpect(
+                    jsonPath("$.message").value(ScrapException.SCRAP_NOT_FOUND.getMessage()));
+        }
+
+        @DisplayName("존재하지 않는 대화주제이면 예외가 발생한다.")
+        @Test
+        void scrapTopicNotFound() throws Exception {
+            // given
+            long scrapId = 1L;
+            long scrapTopicId = 1L;
+
+            willThrow(new GeneralException(ScrapException.SCRAP_TOPIC_NOT_FOUND))
+                .given(scrapService).deleteScrapTopic(scrapId, scrapTopicId);
+
+            // when
+            ResultActions resultActions = mockMvc.perform(
+                delete("/users/scraps/{scrapId}/topics/{scrapTopicId}", scrapId, scrapTopicId));
+
+            // then
+            resultActions.andExpect(status().isBadRequest())
+                .andExpect(
+                    jsonPath("$.message").value(ScrapException.SCRAP_TOPIC_NOT_FOUND.getMessage()));
+        }
     }
 }
 
--- src/test/java/org/example/tokpik_be/scrap/service/ScrapServiceTest.java
+++ src/test/java/org/example/tokpik_be/scrap/service/ScrapServiceTest.java
@@ -3,11 +3,8 @@ package org.example.tokpik_be.scrap.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.mock;
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -98,7 +95,7 @@ public class ScrapServiceTest {
     
     @DisplayName("스크랩 목록을 조회할 수 있다.")
     @Test
-    void getScraps(){
+    void getScraps() {
         // given
         long userId = 1L;
         User user = new User("ex@example.com", "https://www.example.com/profile-photo");
@@ -194,7 +191,7 @@ public class ScrapServiceTest {
 
         @DisplayName("스크랩이 존재하지 않으면 예외가 발생한다.")
         @Test
-        void invalidScrap(){
+        void invalidScrap() {
             // given
             long invalidScrapId = 1L;
             given(scrapRepository.findById(invalidScrapId)).willReturn(Optional.empty());
@@ -208,7 +205,7 @@ public class ScrapServiceTest {
 
         @DisplayName("스크랩에 포함되지 않은 대화주제를 페이징에 사용하면 예외가 발생한다.")
         @Test
-        void invalidTalkTopic(){
+        void invalidTalkTopic() {
             // given
             long scrapId = 1L;
             Scrap scrap = mock(Scrap.class);
@@ -230,7 +227,7 @@ public class ScrapServiceTest {
 
     @DisplayName("스크랩의 개수를 조회할 수 있다.")
     @Test
-    void getScrapCount(){
+    void getScrapCount() {
         // given
         long userId = 1L;
         User user = new User("ex@example.com", "https://www.example.com/profile-photo");
@@ -247,7 +244,7 @@ public class ScrapServiceTest {
 
     @DisplayName("전체 스크랩에 포함된 총 대화주제 개수를 조회할 수 있다.")
     @Test
-    void getScrapTopicCount(){
+    void getScrapTopicCount() {
         // given
         long userId = 1L;
         User user = new User("ex@example.com", "https://www.example.com/profile-photo");
@@ -260,5 +257,65 @@ public class ScrapServiceTest {
 
         // then
         assertThat(response.count()).isEqualTo(20L);
+    }
+
+    @DisplayName("사용자는 스크랩을 삭제할 수 있다.")
+    @Test
+    void deleteScrap() {
+        // given
+        long scrapId = 1L;
+        Scrap scrap = mock(Scrap.class);
+        given(scrapRepository.findById(scrapId)).willReturn(Optional.of(scrap));
+
+        // when
+        scrapService.deleteScrap(scrapId);
+
+        // then
+        verify(scrapRepository).delete(any(Scrap.class));
+    }
+
+    @Nested
+    @DisplayName("스크랩된 대화주제 삭제 시 ")
+    class DeleteScrapTopicTest {
+
+        @DisplayName("성공한다.")
+        @Test
+        void deleteScrapTopic() {
+            // given
+            long scrapId = 1L;
+            Scrap scrap = mock(Scrap.class);
+            given(scrapRepository.findById(scrapId)).willReturn(Optional.of(scrap));
+
+            long scrapTopicId = 1L;
+            ScrapTopic scrapTopic = mock(ScrapTopic.class);
+            given(scrapTopicRepository.findById(scrapTopicId)).willReturn(Optional.of(scrapTopic));
+
+            given(scrap.getScrapTopics()).willReturn(List.of(scrapTopic));
+
+            // when
+            scrapService.deleteScrapTopic(scrapId, scrapTopicId);
+
+            // then
+            verify(scrapTopicRepository).delete(any(ScrapTopic.class));
+        }
+
+        @DisplayName("스크랩에 포함되지 않은 대화주제이면 예외가 발생한다.")
+        @Test
+        void invalidScrapTopic() {
+            // given
+            long scrapId = 1L;
+            Scrap scrap = mock(Scrap.class);
+            given(scrapRepository.findById(scrapId)).willReturn(Optional.of(scrap));
+
+            long invalidScrapTopicId = 1L;
+            ScrapTopic scrapTopic = mock(ScrapTopic.class);
+            given(scrapTopicRepository.findById(invalidScrapTopicId)).willReturn(Optional.of(scrapTopic));
+
+            // when & then
+            assertThatThrownBy(() -> scrapService.deleteScrapTopic(scrapId, invalidScrapTopicId))
+                .isInstanceOf(GeneralException.class)
+                .extracting("exception")
+                .isEqualTo(ScrapException.INVALID_SCRAP_TOPIC);
+        }
     }
 }
